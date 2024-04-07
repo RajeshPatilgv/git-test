74bb337c57ab07909356c5a0cf8fbe224a97a5b8
Rajeshpatelgv <rajeshpatelgv@gmail.com>
2024-04-07 18:56:05 +0530

    first commit

---
+++ python-workspace/week-2-programs.py
@@ -0,0 +1,62 @@
+
+# stmt = "Hare Krishna Hare rama"
+
+# reverse a string
+# print(stmt[::-1]) # entire string get reversed. (anhsirK eraH)
+
+# output: eraH anhsirK
+#         eraH anhsirK
+
+# def reverse_string(string: str) -> str:
+#     return string[::-1]
+
+# wrd_lst = stmt.split()
+# print(wrd_lst)
+# result = ""
+
+# for word in wrd_lst:
+#     result = result + " "+reverse_string(word)
+
+# print(result)
+
+
+# get sum of previous, stop when enter 0.
+nums = []
+while True:
+    num = int(input('enter the numebr'))
+    if num == 0:
+        break
+    nums.append(num)
+
+print(nums)
+print(sum(nums))
+
+# celsius to fahrenheit
+C = 90
+formula = f"{C}*(9/5)+32"
+
+print(eval(formula))
+
+
+# get second highest num in a list.
+def get_highest_num(nums: list[int]):
+    largest = nums[0]
+    second_largest = nums[1]
+    for i in range(2, len(nums)):
+        if largest < nums[i]:
+            largest, second_largest = nums[i], largest
+        elif second_largest < largest and nums[i] > second_largest:
+            second_largest = nums[i]
+
+    return second_largest, largest
+
+
+nums = [11, 2, 3, 9, 0, 7]
+second_high, highest = get_highest_num(nums)
+print("second highest: ", second_high)
+print('highest in list is: ', highest)
+
+# 2nd approach
+# nums.sort()
+# print(nums[-2]) # 2nd max
+# print(nums[-1]) # highest
---
+++ week-1-programs.py
@@ -0,0 +1,94 @@
+
+# a: int = int(input('first value: '))
+# b: int = int(input('second value: '))
+
+# print(a+b)
+
+# for i in range(50):
+#     print(i, end=' ')
+
+# # write a program to print from 1000 to 1010
+# for i in range(1000, 1010, 1):
+#     print(i, end='\t')
+
+# # even numbers
+# nums = [2,6,34,32,11, 9, 15, 22]
+
+# for i in nums:
+#     if i % 2 != 0:
+#         print(i)
+
+# for i in range(len(nums)):
+#     print(i, nums[i])
+
+
+# fruits = ['apple', 'banana', 'mango', 'orange']
+
+# for fruit in fruits:
+#     if 'e' not in fruit:
+#         print(fruit)
+#     elif 'a' not in fruit:
+#         print(fruit)
+
+# value = -10
+
+# if value < 15 and value > 0:
+#     print('inside if')
+# else:
+#     print('else block')
+
+
+# 12345 o/p: 54321
+
+# string
+num = 12345
+# num_str = str(12345)
+
+# result = int(num_str[::-1])
+
+# # print(result, type(result))
+
+
+# result = 0
+
+# while num > 0:
+#     result = result * 10 + num % 10
+#     num = num //10
+
+# print(result)
+
+
+
+result = ""
+
+while num > 0:
+    result = result + str(num % 10)
+    # result += num % 10
+    num = num //10
+
+print(int(result))
+
+
+
+# sort a list.
+
+nums = [20, 30, 50, 10, 90]
+
+# write a program to sort my list
+
+# 1.approch
+# nums.sort(reverse=False)
+
+# 2nd approach
+# result = sorted(nums, reverse=True)
+
+
+# 3rd approach 
+# for i in range(len(nums)):
+#     for j in range(i+1, len(nums)):
+#           first = nums[i]
+#           second = nums[j]
+#         if first > second:
+#             nums[i], nums[j] = nums[j], nums[i]
+
+print(nums)
